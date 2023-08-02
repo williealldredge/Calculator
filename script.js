caa96613233024cad8e731c80e16c7bf287a8701
Willie Alldredge <williealldredge@yahoo.com>
2023-08-02 13:08:56 -0700

    screen, number, operator work

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-let a = 0;
+let a = "";
+let b = "";
 let operator = "";
-let b = 0;
+const numberButton = document.querySelectorAll(".number");
+const clearButton = document.getElementsByClassName("clear");
+const operatorButton = document.querySelectorAll(".operator");
+const currentScreen = document.querySelector(".current");
+const previousScreen = document.querySelector(".previous");
+const equals = document.querySelector(".equals");
+
+
+numberButton.forEach((number) => {
+    number.addEventListener("click", () => {
+        num = number.innerText;
+        a = num;
+        currentScreen.innerHTML += a;
+    })
+})
+
+operatorButton.forEach((op) => {
+    op.addEventListener("click", () => {
+        operator = op.innerText;
+        previousScreen.innerHTML = currentScreen.innerText;
+        currentScreen.innerHTML = b;
+
+    })
+    
+})
+
+
+
+
+
+const operate = (a, b, operator) => {
+   if (operator == "+"){
+    return add(a, b);
+   } else if(operator == "-"){
+    return subtract(a, b);
+   } else if (operator == "*"){
+    return multiply(a, b);
+   } else if (operator == "/"){
+    return (divide(a, b));
+   }
+}
 
 const add = (a, b) => {
     return a + b;
 }
+
 const subtract = (a, b) => {
     return a - b;
 }
@@ -17,14 +59,3 @@ const divide = (a, b) => {
     return a / b;
 }
 
-const operate = (a, b) => {
-    if ( operator === "+"){
-        return add;
-    } else if (operator == "-"){
-        return subtract;
-    } else if (operator == "*"){
-        return multiply;
-    } else if (operator == "/"){
-        return divide;
-    }
-}
